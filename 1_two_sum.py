7029d2ad172d1ee2717088ae876c3d4d0e3f442d
fanruifu <fanruifu@gmail.com>
2020-11-22 21:26:56 -0800

    initial

---
+++ 1_two_sum.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 22 15:14:23 2020
+
+@author: ffr
+"""
+class Solution(object):
+    def twoSum(self, nums, target): 
+        hash_map={}
+        for index, value in enumerate(nums):
+            hash_map[value]=index
+        for index1,value in enumerate(nums):
+            if target - value in hash_map:
+                index2=hash_map[target-value]
+                if index1 != index2:
+                    return [index1,index2]
