613914c61f77be690ba7f6095eade9f3c694a89d
fanruifu <fanruifu@gmail.com>
2020-11-22 21:40:14 -0800

    intial_2

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -14,3 +14,14 @@ class Solution(object):
                 index2=hash_map[target-value]
                 if index1 != index2:
                     return [index1,index2]
+                
+
+# =============================================================================
+# method 2                
+# =============================================================================
+class Solution(object):
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j]==target:
+                    return[i,j]
